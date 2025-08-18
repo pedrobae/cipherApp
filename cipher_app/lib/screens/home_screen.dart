e4c78e642865b798014e4142e32532f5feef63d9
Pedro Abe NH <pedrobettiolabe@gmail.com>
2025-08-18 11:57:54 -0300

    search bar

--- cipher_app/lib/screens/home_screen.dart
+++ cipher_app/lib/screens/home_screen.dart
@@ -1,17 +1,49 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isSearching = false;
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cipher App'),
+        title: _isSearching
+            ? TextField(
+                controller: _searchController,
+                decoration: const InputDecoration(
+                  hintText: 'Procure Cifras...',
+                  border: InputBorder.none,
+                ),
+                onChanged: (value) {
+                  print('Searching: $value');
+                },
+              )
+            : const Text('Cipher App'),
         actions: [
           IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () => Navigator.pushNamed(context, '/settings'),
+            icon: Icon(_isSearching ? Icons.close : Icons.search),
+            onPressed: () => {
+              setState(() {
+                _isSearching = !_isSearching;
+                if (!_isSearching) {
+                  _searchController.clear();
+                }
+              }),
+            },
           ),
         ],
       ),

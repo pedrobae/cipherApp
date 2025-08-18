df71ad1e1d2dfefc3e6ca1be7ef60783f5b860fe
Pedro Abe NH <pedrobettiolabe@gmail.com>
2025-08-18 11:21:23 -0300

    first screens

---
+++ cipher_app/lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cipher App'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () => Navigator.pushNamed(context, '/settings'),
+          ),
+        ],
+      ),
+      body: const Center(child: Text('Home Screen')),
+    );
+  }
+}
---
+++ cipher_app/lib/screens/settings_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cipher App'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () => Navigator.pushNamed(context, '/settings'),
+          ),
+        ],
+      ),
+      body: const Center(child: Text('Settings Screen')),
+    );
+  }
+}

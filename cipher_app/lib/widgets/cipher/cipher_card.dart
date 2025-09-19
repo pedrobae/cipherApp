9eb7bd73ce6eade0cc1f71bafe4ab784582812e2
Pedro Abe NH <pedrobettiolabe@gmail.com>
2025-09-19 12:13:27 -0300

    Refactor CipherCard layout and enhance version selection functionality

--- cipher_app/lib/widgets/cipher/cipher_card.dart
+++ cipher_app/lib/widgets/cipher/cipher_card.dart
@@ -1,4 +1,5 @@
 import 'package:cipher_app/models/domain/cipher/version.dart';
+import 'package:cipher_app/screens/cipher_editor.dart';
 import 'package:flutter/material.dart';
 import '../../models/domain/cipher/cipher.dart';
 import 'tag_chip.dart';
@@ -22,71 +23,130 @@ class CipherCard extends StatelessWidget {
     return Card(
       color: Theme.of(context).colorScheme.surfaceContainerHighest,
       elevation: 4,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ExpansionTile(
-          key: PageStorageKey(cipher.id),
-          initiallyExpanded: isExpanded,
-          onExpansionChanged: (expanded) {
-            if (expanded) onExpand;
-          },
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Wrap(
-                      crossAxisAlignment: WrapCrossAlignment.end,
-                      spacing: 10,
-                      children: [
-                        Text(
-                          cipher.title,
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        Text(cipher.author),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text('Tom: ${cipher.musicKey}'),
-                        const SizedBox(width: 8),
-                        Text('Tempo: ${cipher.tempo}'),
-                      ],
-                    ),
-                    const SizedBox(height: 8),
-                    Wrap(
-                      spacing: 4,
-                      runSpacing: 4,
-                      children: cipher.tags
-                          .map((tag) => TagChip(tag: tag))
-                          .toList(),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          children: cipher.maps.map((version) {
-            return InkWell(
-              onTap: () => selectVersion?.call(version, cipher),
-              child: Row(
-                spacing: 16,
+      child: ExpansionTile(
+        shape: Border(),
+        childrenPadding: EdgeInsets.only(left: 8, right: 8, bottom: 8),
+        key: PageStorageKey(cipher.id),
+        initiallyExpanded: isExpanded,
+        onExpansionChanged: (expanded) {
+          if (expanded) onExpand;
+        },
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    version.versionName!,
-                    style: Theme.of(context).textTheme.titleMedium,
+                  Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.end,
+                    spacing: 10,
+                    children: [
+                      Text(
+                        cipher.title,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      Text(cipher.author),
+                    ],
                   ),
-                  Text(
-                    'Tom: ${version.transposedKey}',
-                    style: Theme.of(context).textTheme.titleSmall,
+                  Row(
+                    children: [
+                      Text('Tom: ${cipher.musicKey}'),
+                      const SizedBox(width: 8),
+                      Text('Tempo: ${cipher.tempo}'),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Wrap(
+                    spacing: 4,
+                    runSpacing: 4,
+                    children: cipher.tags
+                        .map((tag) => TagChip(tag: tag))
+                        .toList(),
                   ),
                 ],
               ),
-            );
-          }).toList(),
+            ),
+          ],
         ),
+        children: [
+          Column(
+            spacing: 4,
+            children: [
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EditCipher(
+                      cipher: cipher,
+                      isNewVersion: true,
+                      startTab: 'version',
+                    ),
+                  ),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Theme.of(context).colorScheme.secondaryContainer,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 16,
+                    children: [
+                      const Icon(Icons.library_music),
+                      Text(
+                        'Criar uma Nova Versão',
+                        style: Theme.of(context).textTheme.titleMedium
+                            ?.copyWith(
+                              color: Theme.of(
+                                context,
+                              ).colorScheme.onTertiaryContainer,
+                              fontWeight: FontWeight.w500,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ...cipher.maps.map((version) {
+                return GestureDetector(
+                  onTap: () => selectVersion?.call(version, cipher),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Theme.of(context).colorScheme.surfaceContainer,
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      spacing: 16,
+                      children: [
+                        Text(
+                          version.versionName!,
+                          style: Theme.of(context).textTheme.titleMedium
+                              ?.copyWith(
+                                color: Theme.of(
+                                  context,
+                                ).colorScheme.onSurfaceVariant,
+                                fontWeight: FontWeight.w500,
+                              ),
+                        ),
+                        Text(
+                          'Tom: ${version.transposedKey}',
+                          style: Theme.of(context).textTheme.titleSmall
+                              ?.copyWith(
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }),
+            ],
+          ),
+        ],
       ),
     );
   }

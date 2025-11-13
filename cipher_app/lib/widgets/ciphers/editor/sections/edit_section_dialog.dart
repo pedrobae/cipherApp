9d22f448f8b5beb57dec3b2f33ec94eaf5545f9b
Pedro Abe <pedrobettiolabe@gmail.com>
2025-11-12 23:22:17 -0300

    feat: Refactor EditSectionDialog to StatefulWidget and enhance UI with color picker

--- cipher_app/lib/widgets/ciphers/editor/sections/edit_section_dialog.dart
+++ cipher_app/lib/widgets/ciphers/editor/sections/edit_section_dialog.dart
@@ -1,7 +1,8 @@
 import 'package:cipher_app/models/domain/cipher/section.dart';
 import 'package:flutter/material.dart';
+import 'package:cipher_app/utils/section_constants.dart';
 
-class EditSectionDialog extends StatelessWidget {
+class EditSectionDialog extends StatefulWidget {
   final Section section;
   final void Function(String?, String?, Color?) onSave;
   final VoidCallback onDelete;
@@ -14,26 +15,129 @@ class EditSectionDialog extends StatelessWidget {
   });
 
   @override
+  State<EditSectionDialog> createState() => _EditSectionDialogState();
+}
+
+class _EditSectionDialogState extends State<EditSectionDialog> {
+  late TextEditingController contentCodeController;
+  late TextEditingController contentTypeController;
+  late Color contentColor;
+  Map<String, Color> availableColors = {};
+
+  @override
+  void initState() {
+    contentCodeController = TextEditingController(
+      text: widget.section.contentCode,
+    );
+
+    contentTypeController = TextEditingController(
+      text: widget.section.contentType,
+    );
+
+    contentColor = widget.section.contentColor;
+
+    List<Color> presentColors = [];
+    for (var entry in defaultSectionColors.entries) {
+      if (!presentColors.contains(entry.value)) {
+        availableColors[predefinedSectionTypes[entry.key]!] = entry.value;
+        presentColors.add(entry.value);
+      }
+    }
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Implementation of the dialog UI goes here
     return AlertDialog(
       title: Text('Edit Section'),
       content: Column(
+        spacing: 8,
         mainAxisSize: MainAxisSize.min,
         children: [
-          // Fields for editing sectionKey, sectionName, and sectionColor
+          TextField(
+            controller: contentCodeController,
+            decoration: InputDecoration(labelText: 'Content Code'),
+          ),
+          TextField(
+            controller: contentTypeController,
+            decoration: InputDecoration(labelText: 'Content Type'),
+          ),
+
+          // Default section colors picker
+          DropdownButtonFormField<Color>(
+            decoration: InputDecoration(labelText: 'Content Color'),
+            selectedItemBuilder: (context) {
+              return availableColors.entries.map((entry) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Row(
+                    spacing: 16,
+                    children: [
+                      Container(
+                        width: 24,
+                        height: 24,
+                        decoration: BoxDecoration(
+                          color: entry.value,
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                      ),
+                      Text(entry.key),
+                    ],
+                  ),
+                );
+              }).toList();
+            },
+            items: [
+              ...availableColors.entries.map(
+                (entry) => DropdownMenuItem<Color>(
+                  value: entry.value,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Row(
+                      spacing: 16,
+                      children: [
+                        Container(
+                          width: 24,
+                          height: 24,
+                          decoration: BoxDecoration(
+                            color: entry.value,
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                        ),
+                        Text(entry.key),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+            onChanged: (Color? newColor) {
+              setState(() {
+                contentColor = newColor!;
+              });
+            },
+          ),
         ],
       ),
       actions: [
-        TextButton(onPressed: onDelete, child: Text('Delete')),
         TextButton(
           onPressed: () {
-            // Call onSave with updated values
-            onSave(
-              section.contentCode,
-              section.contentType,
-              section.contentColor,
+            widget.onDelete();
+            Navigator.of(context).pop();
+          },
+          child: Text('Delete'),
+        ),
+        TextButton(
+          onPressed: () {
+            widget.onSave(
+              contentCodeController.text,
+              contentTypeController.text,
+              contentColor,
             );
+            Navigator.of(context).pop();
           },
           child: Text('Save'),
         ),
